5eb78eeda6b1962daad362d50ac4132f25ab687a
Roberto Fischer <roberto.fischer@synaptic.cl>
2016-10-20 15:38:50 -0300

    ADDED: completed the cisco section

--- base.py
+++ base.py
@@ -13,14 +13,17 @@ def main(argv):
     dirty_commands = ''
 
     # Building the argument parser
-    parser = argparse.ArgumentParser(description, 'Send commands to network devices')
+    parser = argparse.ArgumentParser(description='Send commands to network devices')
+    parser.add_argument("-e", "--enable", action="store", dest="enable",
+                        help="The user's enable password if different from regular password")
     required = parser.add_argument_group('Required Arguments')
-    required.add_argument("-d", "--device", action="store", dest="hosttype", choices=["cisco", "dell", "hp", "a10"], help="Choose a device to run commands against")
+    required.add_argument("-d", "--device", action="store", dest="hosttype", choices=["cisco", "dell", "hp", "a10"],
+                          help="Choose a device to run commands against")
     required.add_argument("-u", "--user", action="store", dest="username", help="Username to login as")
     required.add_argument("-p", "--pass", action="store", dest="password", help="The user's password")
-    parser.add_argument("-e", "--enable", action="store", dest="enable", help="The user's enable password")
     required.add_argument("-h", "--host", action="store", dest="hostname", help="The remote host")
-    required.add_argument("-c", "--command", action="store", dest="dirty_commands", help="Commands to be run on the remote device. Separate multiple commands with ;")
+    required.add_argument("-c", "--command", action="store", dest="dirty_commands",
+                          help="Commands to be run on the remote device. Separate multiple commands with ;")
 
     # Splitting the commands into an array
     commands = dirty_commands.split(";")
@@ -40,21 +43,45 @@ def main(argv):
         sys.quit()
 
 
-
-def cisco(user, pass, enable, hostname, commands=[]):
+def cisco(user, passw, enable, hostname, commands=[]):
     pass
-    conn = paramiko.SSHClient()
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname, username=user, password=passw, look_for_keys=False,
+                allow_agent=False)
+    ssh_conn = ssh.invoke_shell()
+    output = ssh_conn.recv(2000)
+
+    if "authentication" in output:
+        ssh_conn.close()
+        return("Connection closed due to incorrect login credentials")
+
+    if enable == "":
+        enable = passw
+
+    if output[-1] == ">":
+        ssh_conn.send(enable)
+        output = ssh_conn.recv(2000)
+        if output[-1] == ">":
+            ssh_conn.close()
+            return("Connection closed due to incorrect enable credentials")
+
+    for cmd in output:
+        ssh_conn.send(cmd + "\n")
+        output = ssh_conn.recv(20000)
+    print output
+    sys.quit()
 
 
-def dell(user, pass, enable, hostname, commands=[]):
+def dell(user, passw, enable, hostname, commands=[]):
     pass
 
 
-def hp(user, pass, enable, hostname, commands=[]):
+def hp(user, passw, enable, hostname, commands=[]):
     pass
 
 
-def a10(user, pass, enable, hostname, commands=[]):
+def a10(user, passw, enable, hostname, commands=[]):
     pass
 
 
